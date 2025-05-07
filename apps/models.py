f5bfd390537666e1d4f6238e9003543b0c2027f6
Santiago Gonzalez <santiagogo1993@gmail.com>
2025-05-07 20:15:39 +0000

    Update environment variables and configure PostgreSQL database settings

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Pregunta(models.Model):
+    pregunta = models.CharField(max_length=255)
+    informacion = models.TextField()
+
+    def __str__(self):
+        return self.pregunta
--- piragua_chat/settings.py
+++ piragua_chat/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps"
 ]
 
 MIDDLEWARE = [
@@ -77,8 +83,12 @@ WSGI_APPLICATION = "piragua_chat.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
