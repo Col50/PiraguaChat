45e5f94abf2a7cf3b768b66cf2483512f5f9d945
Santiago Gonzalez <santiagogo1993@gmail.com>
2025-05-13 12:53:17 +0000

    Fix translation in USER_TYPE_CHOICES: change "Persona" to "Human"

--- piragua_chat/models/history_message.py
+++ piragua_chat/models/history_message.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class History_Message(models.Model):
     USER_TYPE_CHOICES = [
-        ("Persona", "Human"),
+        ("Human", "Human"),
         ("AI", "AI"),
     ]
     phone_number = models.CharField(max_length=20)
